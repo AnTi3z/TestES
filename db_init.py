6d2575b2fde246cad6b5944e31294771aa8abc3c
AnTi3z <anti3z@anti3z.ru>
2021-10-08 14:07:03 +0300

    Make db_init script runnable

--- db_init.py
+++ db_init.py
@@ -6,10 +6,18 @@ from app import db
 
 
 def import_from_csv(filename):
+    print("Creating DB...")
     db.create_all()
     with open(filename, encoding='utf-8') as csvfile:
         data_reader = csv.DictReader(csvfile)
+        print("Importing data from .csv file...")
         for row in data_reader:
             row['created_date'] = datetime.fromisoformat(row['created_date'])
             db.session.add(Document(**row))
+        print("Save data to new DB...")
         db.session.commit()
+        print("Complete")
+
+
+if __name__ == "__main__":
+    import_from_csv("posts.csv")
