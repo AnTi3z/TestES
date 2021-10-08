e436ddb03cc37489d524ef0ba7d0dcb3ba7359f1
AnTi3z <anti3z@anti3z.ru>
2021-10-08 15:35:34 +0300

    Add SQLALCHEMY_TRACK_MODIFICATIONS config

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 ELASTICSEARCH_URL = "http://localhost:9200"
 
 db = SQLAlchemy(app)
 es = Elasticsearch(ELASTICSEARCH_URL)
+
+from app import routes
