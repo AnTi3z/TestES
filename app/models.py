c6bd4686696fec08d576a0406237364b7e55bf69
AnTi3z <anti3z@anti3z.ru>
2021-10-11 00:23:34 +0300

    Add asynchronousity

--- app/models.py
+++ app/models.py
@@ -6,8 +6,8 @@ from app.search import add_to_index, remove_from_index, query_index
 
 class SearchableMixin:
     @classmethod
-    def search(cls, expression: str, page: int = 1, per_page: int = 10000):
-        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+    async def search(cls, expression: str, page: int = 1, per_page: int = 10000):
+        ids, total = await query_index(cls.__tablename__, expression, page, per_page)
         if total == 0:
             return cls.query.filter_by(id=0), 0
         when = []
@@ -16,7 +16,7 @@ class SearchableMixin:
         return cls.query.filter(cls.id.in_(ids)).order_by(db.case(when, value=cls.id)), total
 
     @classmethod
-    def before_commit(cls, session):
+    async def before_commit(cls, session):
         session._changes = {
             'add': list(session.new),
             'update': list(session.dirty),
@@ -24,22 +24,22 @@ class SearchableMixin:
         }
 
     @classmethod
-    def after_commit(cls, session):
+    async def after_commit(cls, session):
         for obj in session._changes['add']:
             if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
+                await add_to_index(obj.__tablename__, obj)
         for obj in session._changes['update']:
             if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
+                await add_to_index(obj.__tablename__, obj)
         for obj in session._changes['delete']:
             if isinstance(obj, SearchableMixin):
-                remove_from_index(obj.__tablename__, obj)
+                await remove_from_index(obj.__tablename__, obj)
         session._changes = None
 
     @classmethod
-    def reindex(cls):
+    async def reindex(cls):
         for obj in cls.query:
-            add_to_index(cls.__tablename__, obj)
+            await add_to_index(cls.__tablename__, obj)
 
 
 event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
--- app/search.py
+++ app/search.py
@@ -1,25 +1,46 @@
 from app import es
 
 
-def add_to_index(index: str, model):
+async def add_to_index(index: str, model):
+    """
+    Добавляет экземпляр модели в индекс
+
+    :param index: Имя индекса
+    :param model: Объект с __searchable__ аттрибутом
+    """
     if not es:
         return
     payload = {}
     for field in model.__searchable__:
         payload[field] = getattr(model, field)
-    es.index(index=index, doc_type=index, id=model.id, document=payload)
+    await es.index(index=index, doc_type=index, id=model.id, document=payload)
 
 
-def remove_from_index(index: str, model):
+async def remove_from_index(index: str, model):
+    """
+    Удаляет экземпляр модели из индекса
+
+    :param index: Имя индекса
+    :param model: Индексированый объект, который необходимо удалить из индекса
+    """
     if not es:
         return
-    es.delete(index=index, doc_type=index, id=model.id)
+    await es.delete(index=index, doc_type=index, id=model.id)
 
 
-def query_index(index: str, query: str, page: int, per_page: int, filter_fields=None):
+async def query_index(index: str, query: str, page: int, per_page: int):
+    """
+    Полнотекстовый поиск документов в индексе
+
+    :param index: Имя индекса
+    :param query: Строка для поиска по проиндексированым полям (должны быть заданы в __searcheable__ аттрибуте модели)
+    :param page: Порядковый номер "страницы"
+    :param per_page: Кол-во результатов на одной "странице"
+    :return: Список из двух элементов: список id найденных документов и кол-во найденых документов
+    """
     if not es:
         return [], 0
-    search = es.search(
+    search = await es.search(
         index=index,
         query={'multi_match': {'query': query, 'fields': ['*']}},
         from_=(page - 1) * per_page,
