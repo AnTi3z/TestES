4f35949e9510c852f260bd1d22fef6bb179bffe1
AnTi3z <anti3z@anti3z.ru>
2021-10-12 15:59:50 +0300

    Improve startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+host="http://localhost:9200"
+
+function elastic_ready {
+  health="$(curl -fsSL "$host/_cat/health?h=status")"
+  if [ "$health" = "yellow" ]; then
+    return 0
+  fi
+  return 1
+}
+
+until elastic_ready; do
+  echo "Waiting for Elasticsearch service up..."
+  sleep 5
+done
+echo "Elasticsearch service ready!"
 
 venv/bin/python db_init.py
 venv/bin/python main.py
